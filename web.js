8826e5720722f3ef0e91f3d0eb16e40827076e6c
neysatifania <152868050+neysatifania@users.noreply.github.com>
2024-01-18 12:03:46 +0700

    Add files via upload

---
+++ web.js
@@ -0,0 +1,12 @@
+console.log("Selamat Anda berhasil menggunakan JavaScript pada Website");
+
+
+function alert(nama) {
+      console.log('Hati-hati, ' + nama);
+    }
+     
+    alert('Chewbacca'); // Output: Hati-hati, Chewbacca
+    // Output di atas akan tercetak ke console browser
+     
+    window.alert('Chewbacca'); // Output: Hati-hati, Chewbacca
+    // Output di atas akan tetap tercetak pada console browser
